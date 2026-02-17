c172ccba6c225c0cd6cf354fb5524bd05e38d601
prudhvi kumar reddy <meet.prudhvi.reddy@gmail.com>
2026-02-16 19:19:16 -0500

    Initial Commit

---
+++ src/main/java/com/example/price_comparator/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.price_comparator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Price Comparator Running Successfully!";
+    }
+}
